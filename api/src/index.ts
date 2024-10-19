74d1b631193fb815cfce52f550fbecee765d9fc7
Pratham Sahu <96994282+Prathamm-sahu@users.noreply.github.com>
2024-10-19 07:58:57 +0000

    created a simple endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,2 +1,19 @@
+import express from "express"
+import { nanoid } from "nanoid"
 import dotenv from "dotenv"
+import { RedisManager } from "./RedisManager"
 dotenv.config()
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.post("/", async (req, res) => {
+  try {
+    const id = nanoid()
+    RedisManager.lpush("engine", id)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.listen(PORT, () => `server started on port: ${PORT}`)
