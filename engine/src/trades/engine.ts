553cabc7f81382d6d4cc57ceee67d10494c6e7ad
Prabhat shukla <98808783+PRABHAT1SHUKLA@users.noreply.github.com>
2024-10-20 18:06:40 +0000

    created balance logic in case of buy

--- engine/src/trades/engine.ts
+++ engine/src/trades/engine.ts
@@ -42,15 +42,21 @@ export class Engine{
     if (snapshot) {
         const snapshotSnapshot = JSON.parse(snapshot.toString());
         this.orderbooks = snapshotSnapshot.orderbooks.map((o: any) => new Orderbook(o.stockSymbol , o.yes , o.no));
-        this.balances = new Map(snapshotSnapshot.balances);
+        this.inrbalances = snapshotSnapshot.inrbalances;
+        this.stockbalances = snapshotSnapshot.stockbalances;
     } else {
         this.orderbooks = [new Orderbook('new' ,{} ,{})];
-        this.setBaseBalances();
+        this.setinrBalances();
+        this.setstockbalances()
     }
     setInterval(() => {
         this.saveSnapshot();
     }, 1000 * 3);
 }
+setinrBalances(){
+  
+}
+
 
 saveSnapshot() {
   const snapshotSnapshot = {
@@ -63,7 +69,52 @@ saveSnapshot() {
 
   createSellOrder(userId:string , quantity:number, price:number , stockSymbol: string , stockType:"yes"|"no"){
     
-     const orderBook  = this.orderbooks.find((o)=> o.orderbook.stockSymbol === stockSymbol)
+     const orderBook  = this.orderbooks.find((o)=> o.stockSymbol === stockSymbol)
+     if(!orderBook){
+      throw new Error("orderbook does not exist")
+     }
+
+     this.checkandLockBalance(userId, quantity, price )
   }
+
+
+  buyBalance(userId:string , quantity: number , price:number){
+      const userBalance = this.inrbalances[userId]
+
+      const requiredBalance =  price * quantity
+      if(!userBalance){
+        throw new Error(" user doesn't exist  ")
+      }
+
+      if(this.inrbalances[userId]?.available!<requiredBalance){
+        throw new Error("Not sufficient balance")
+      }else{
+        this.inrbalances[userId]!.available-=requiredBalance
+        this.inrbalances[userId]!.locked+=requiredBalance
+      }
+ }
+
+  onRamp(userId: string, amount: number) {
+    const userBalance = this.inrbalances[userId];
+    if (!userBalance) {
+        this.inrbalances[userId]={
+          locked:0, 
+          available:0
+        }
+    } else {
+        userBalance.available += amount;
+    }
+}
+
+onMint(userId: string , amount:number , stockSymbol: string){
+ 
+  const orderBook  = this.orderbooks.find((o)=> o.stockSymbol === stockSymbol)
+  if(!orderBook){
+   throw new Error(`orderbook with ${stockSymbol} does not exist`)
+  }
+
+   
+
+}
 
 }
