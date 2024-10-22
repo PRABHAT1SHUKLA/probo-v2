f0f330a8e63eb0855d97e6c7fe13853e6312bfed
prabhat <prabhatshukla0849@gmail.com>
2024-10-22 18:07:34 +0530

    sell order endpoint implemented

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { RedisManager } from "./RedisManager"
-import { CREATE_USER, ONRAMP } from "./types/to"
+import { CREATE_USER, ONRAMP, SELL_ORDER } from "./types/to"
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
@@ -9,33 +9,42 @@ const app = express()
 
 
 
-app.post("/user/create/:userId", async(req, res)=>{
+app.post("/user/create/:userId", async (req, res) => {
   const userId = req.params.userId
 
   const response = await RedisManager.getInstance().sendAndAwait({
     type: CREATE_USER,
-    data:{
-      userId:userId
+    data: {
+      userId: userId
     }
-})
+  })
 })
 
-app.post("/onramp/inr" ,async(req,res)=>{
-  const {userId , amount} = req.body
+app.post("/onramp/inr", async (req, res) => {
+  const { userId, amount } = req.body
   let num = Number(amount)
-  const response  = await RedisManager.getInstance().sendAndAwait({
+  const response = await RedisManager.getInstance().sendAndAwait({
     type: ONRAMP,
-    data:{
+    data: {
       userId,
       amount
     }
   }
-    )
+  )
+  res.json(response.payload)
 })
 
-app.post("/order/sell" , async(req, res)=>[
- 
-])
+app.post("/order/sell", async (req, res) => {
+  const { userId, stockSymbol, quantity, price, stockType } = req.body
+  const response = await RedisManager.getInstance().sendAndAwait({
+    type: "SELL_ORDER",
+    data: {
+      userId, stockSymbol, quantity, price, stockType
+
+    }
+  })
+  res.json(response.payload)
+})
 
 
 app.listen(PORT, () => console.log(`server started on port: ${PORT}`))
