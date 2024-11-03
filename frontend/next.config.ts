00a567d578c423966c1cd8f55b594e04dca16a95
Pratham Sahu <prathamsahu051@gmail.com>
2024-11-04 01:26:30 +0530

    feat: updated next configuration files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "probo.in"
+      },
+      {
+        protocol: "https",
+        hostname: "probo.gumlet.io"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
